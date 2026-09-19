76e19855a3fcf3192f9f5f865955628a9a012269
Edipeni media <mediaedipeni@gmail.com>
2026-09-19 13:11:36 +0700

    Delete sw.js

--- sw.js
+++
@@ -1,4 +0,0 @@
-const CACHE_NAME="edipeni-admin-v2";
-self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll(["./","./index.html","./manifest.json"]))));
-self.addEventListener("activate",e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE_NAME).map(k=>caches.delete(k))))));
-self.addEventListener("fetch",e=>{if(e.request.method!=="GET")return;e.respondWith(fetch(e.request).then(r=>{const c=r.clone();caches.open(CACHE_NAME).then(x=>x.put(e.request,c));return r}).catch(()=>caches.match(e.request)))});
